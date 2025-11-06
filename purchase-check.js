b853ba437cdd23acad76ee6f1a6dc2d14e108650
Lauris Bakanauskis <llaurisb07@gmail.com>
2025-11-06 08:59:51 +0200

    lidz 3.uzd

---
+++ purchase-check.js
@@ -0,0 +1,8 @@
+let cartTotal = 12;
+
+if (cartTotal >= 80.00){
+    console.log("Brīdinājums: Jūsu pasūtījums pārsniedz 80 €. Pārskatiet preces.");
+}
+else {
+    console.log("Pasūtījums ir zem brīdinājuma sliekšņa – turpiniet veikt pirkumu.");
+}
---
+++ while-loop.js
@@ -0,0 +1,15 @@
+let i = 11;
+let j = 20;
+let h = 0;
+
+while (i <= 20) {
+    let h = i
+    console.log(h)
+    i++;
+}
+
+while (j >= 11) {
+    let h = j
+    console.log(h)
+    j--;
+}
